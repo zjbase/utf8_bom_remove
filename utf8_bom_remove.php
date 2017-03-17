5fa2eeb8fd70d70b514f1bef6aebdcdf66e86a15
zjpro <52linux@users.noreply.github.com>
2017-03-17 14:33:38 +0800

    Update utf8_bom_remove.php

--- utf8_bom_remove.php
+++ utf8_bom_remove.php
@@ -4,24 +4,47 @@ $basedir=$_GET['dir'];
 }else{
 $basedir = '.';
 }
-checkdir($basedir);
+$counter = 0 ;  
+if(isset($_GET['w']) && $_GET['w']==1)
+{
+	checkdir($basedir,true);
+}else{
+	checkdir($basedir);
+}
 
 
-function checkdir($basedir){
+
+function checkdir($basedir,$erasebom=false){
+
+ 	global $counter;
 	if ($dh = opendir($basedir)) {
 		while (($file = readdir($dh)) !== false) {
 			if ($file != '.' && $file != '..'){
-				if (!is_dir($basedir."/".$file)) {
-					echo "filename: $basedir/$file ".checkBOM("$basedir/$file")." <br>";
+				 
+				if (!is_dir($basedir.DIRECTORY_SEPARATOR.$file)) {
+					 
+					$checkbom_r = checkBOM($basedir.DIRECTORY_SEPARATOR . $file);
+					if($checkbom_r==0) {
+						$counter++;
+						if($erasebom){
+ 							rewrite ($basedir. DIRECTORY_SEPARATOR .$file);
+							echo $basedir . DIRECTORY_SEPARATOR . $file  . " bom found , erase bom ok <br/> " . PHP_EOL;
+						}else{
+							echo $basedir . DIRECTORY_SEPARATOR . $file  . " bom found <br/> ". PHP_EOL ;	
+						}
+					}
+					
 				}else{
-					$dirname = $basedir."/".$file;
-					checkdir($dirname);
+					$dirname = $basedir. DIRECTORY_SEPARATOR .$file;
+					checkdir($dirname,$erasebom);
 				}
 			}
 		}
 	closedir($dh);
 	}
 }
+
+echo "<br/> all done , total counter is " . $counter;
 function checkBOM ($filename) {
 	 
 	 
@@ -31,21 +54,17 @@ function checkBOM ($filename) {
 		$charset[2] = substr($contents, 1, 1);
 		$charset[3] = substr($contents, 2, 1);
 		if (ord($charset[1]) == 239 && ord($charset[2]) == 187 && ord($charset[3]) == 191) {
-			 
-			$rest = substr($contents, 3);
-				if(isset($_GET['w'])){
-					rewrite ($filename, $rest);
-					return ("<font color=red>BOM found, automatically removed.</font>");
-				}else{
-					return ("<font color=red>BOM found, NOT automatically removed.</font>");
-				}
-			 
-		}else {return ("BOM Not Found.");}
+			return 0;
+		}else{
+			return 2;
+		}
 	}else{
-		return ("not php file");
+		return 1;
 	}
 }
-function rewrite ($filename, $data) {
+function rewrite ($filename) {
+	echo $filename;
+	$data = substr(file_get_contents($filename),3);
     $filenum = fopen($filename, "w");
     flock($filenum, LOCK_EX);
     fwrite($filenum, $data);
